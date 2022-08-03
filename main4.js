e08bdc3e459afaf49c0a800a545d18c0e3aac214
mariyakhno <marinayakhno31@gmail.com>
2022-08-03 17:43:17 +0200

    Initial commit

---
+++ main4.js
@@ -0,0 +1,26 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// let i = 0;
+// for (i < 10; i++) {
+//     document.write(`<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>`);
+// }
+
+
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+//     let i = 0;
+// for (i < 10; i++) {
+//     document.write(`<div>${i}- Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>`);
+// }
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// let i = 0;
+// while (i<20) {
+//     document.write(`<h1>Lorem ipsum dolor sit amet.</h1>`);
+//     i++;
+}
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+let b = 0;
+while (b < 20) {
+    document.write(`<h1>${b} - Lorem ipsum dolor sit amet.</h1>`)
+    b++;
